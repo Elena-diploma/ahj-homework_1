ab68b1fa69db1df67d28724693eee0ccfd95a431
Elena Tursunova <elenatursunova@gmail.com>
2022-01-24 23:11:51 +0300

    Корректировка сборки проекта

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,12 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 module.exports = merge (common, {
     mode: 'production',
     optimization: {
-        minimize: true,
-        minimizer: [new OptimizeCSSAssetsPlugin({})]
-    }
+        minimize: false,
+        minimizer: [new CssMinimizerPlugin()]
+    },
+  plugins: [new MiniCssExtractPlugin()],
 });
